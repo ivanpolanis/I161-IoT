26714ecfaf98efd536ab9920336ee9a2615dd23d
Iván Valentín Polanis <polanis.ivan@alu.ing.unlp.edu.ar>
2026-04-25 11:45:39 -0300

    update DHT sensor type to DHT11; initialize LED control; remove unused server.cpp file

--- TP1/TP1.ino
+++ TP1/TP1.ino
@@ -2,10 +2,11 @@
 #include "src/dht.h"
 #include "src/routes.h"
 #include "src/spiffs_manager.h"
+#include "src/led_control.h"
 #include <WebServer.h>
 
 #define DHTPIN 25
-#define DHTTYPE DHT22
+#define DHTTYPE DHT11
 
 WebServer server(80);
 
@@ -14,6 +15,7 @@ void setup() {
   Serial.begin(115200);
   initializeDHT(DHTPIN, DHTTYPE);
   initializeWifi();
+  initializeLed();
 
   if (!initSPIFFS()) {
     Serial.println("Fatal: SPIFFS init failed");
@@ -27,5 +29,6 @@ void setup() {
 void loop() {
   server.handleClient();
 }
+
 
 
