46c5d4a68e7f68c79be01ccaf17d8a84f6d9a578
Iván Valentín Polanis <polanis.ivan@alu.ing.unlp.edu.ar>
2026-06-18 15:28:08 -0300

    borramos esp

--- TP_Integrador/esp32/src/rfid_reader.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-void initRfid();
-
-// Devuelve true y escribe el UID en hex mayúsculas si hay una tarjeta presente.
-// Retorna false si no hay tarjeta o si hubo un error de lectura.
-bool readUid(String& uid);
--- TP_Integrador/esp32/src/wifi_mgr.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void initWifi();
-void handleWifi();
